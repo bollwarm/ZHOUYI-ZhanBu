00ca026bd0b71bad008ce45b9f6eddf760bfd236
Orange <bollwarm@tom.com>
2016-12-06 16:20:02 +0800

    修复输出部分字符乱码

--- example/zhanpu.pl
+++ example/zhanpu.pl
@@ -1,6 +1,4 @@
      use Encode; 
      use ZHOUYI::ZhanPu;
-      
     my ( $gnum, $bgnum, $byao, $bgua ) = qigua();
-   print jiegua(qigua()); 
    print Encode::encode("utf8",jiegua( $gnum, $bgnum, $byao, $bgua ));
